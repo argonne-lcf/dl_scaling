1f00138bec7b6b33b5209137204c88b88f68bbe3
Riccardo Balin <balin@x4717c1s7b0n0.hostmgmt.cm.aurora.alcf.anl.gov>
2025-06-24 21:23:03 +0000

    Update alltoallv example with oneCCL

---
+++ aurora_frameworks_scaling/c++-sycl-oneccl-all2allv/affinity.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+num_gpus=12
+gpu_id=$((PALS_LOCAL_RANKID % num_gpus ))
+export ZE_AFFINITY_MASK=$gpu_id
+echo $PALS_LOCAL_RANKID running on GPU $gpu_id
+exec "$@"
--- aurora_frameworks_scaling/c++-sycl-oneccl-all2allv/build.sh
+++ aurora_frameworks_scaling/c++-sycl-oneccl-all2allv/build.sh
@@ -2,7 +2,7 @@
 
 module load frameworks
 mpicxx -o all2allv_ccl all2allv_ccl.cpp -fsycl -lmpi \
-    -I/opt/aurora/24.180.3/updates/oneapi/ccl/2021.13.1_20240808.145507/include \
-    -L/opt/aurora/24.180.3/updates/oneapi/ccl/2021.13.1_20240808.145507/lib -lccl
+    -I/opt/aurora/24.347.0/oneapi/ccl/2021.14/include \
+    -L/opt/aurora/24.347.0/oneapi/ccl/2021.14/lib -lccl
 
 
